b1bc7be3f36d85a47fa7fb47b2224da3172d6eb1
redbirdfan <redbirdfan38@gmail.com>
2022-12-07 21:31:40 -0600

    exercise done

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Movie still shots!';
+  image1 = 'https://www.themoviedb.org/t/p/original/2PbESrE0vAPghgE62o7Xoo6l2gc.jpg';
+  image2 = 'https://media.wired.com/photos/5ca648a330f00e47fd82ae77/master/pass/Culture_Matrix_Code_corridor.jpg';
+  image3 = 'https://www.healthypawspetinsurance.com/blog/wp-content/uploads/2019/12/the-grinch.png';
 
   constructor() { }
 
